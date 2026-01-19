c027be76272e7b46af082fc426d8cec634188bc9
krishnahasare <krishnahasare@gmail.com>
2026-01-19 17:07:42 +0530

    main py updated

--- app/main.py
+++ app/main.py
@@ -1,24 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.recommend import router as recommend_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="AI Outfit Recommendation Engine",
     version="1.0"
 )
 
-# ✅ CORS (Local + Vercel)
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://outfit-recommendation-engine-e4qn.vercel.app/"  # 🔁 CHANGE to your real Vercel URL
+        "https://outfit-recommendation-engine-e4qn.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # Routes
 app.include_router(recommend_router, prefix="/recommendations")
